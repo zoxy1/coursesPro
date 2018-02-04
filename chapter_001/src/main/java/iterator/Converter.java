af1013833e917992b60187baf8ac855306763db8
zoxy1 <zoxy1@yandex.ru>
2018-02-05 03:09:35 +0700

    add Converter class and test

---
+++ chapter_001/src/main/java/iterator/Converter.java
@@ -0,0 +1,61 @@
+package iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Converter {
+    private Iterator<Integer> currentIterator;
+    private Boolean firstIterate = true;
+
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            private void saveIteratorIfFirstIterate() {
+                if (firstIterate) {
+                    currentIterator = it.next();
+                    firstIterate = false;
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                Boolean isFound = false;
+
+                saveIteratorIfFirstIterate();
+                if (currentIterator != null && currentIterator.hasNext()) {
+                    isFound = true;
+                } else {
+                    while (it.hasNext()) {
+                        currentIterator = it.next();
+                        if (currentIterator != null && currentIterator.hasNext()) {
+                            isFound = true;
+                            break;
+                        }
+                    }
+                }
+                return isFound;
+            }
+
+            @Override
+            public Integer next() {
+                Integer value = null;
+
+                saveIteratorIfFirstIterate();
+                if (currentIterator != null && currentIterator.hasNext()) {
+                    value = currentIterator.next();
+                } else {
+                    while (it.hasNext()) {
+                        currentIterator = it.next();
+                        if (currentIterator != null && currentIterator.hasNext()) {
+                            value = currentIterator.next();
+                            break;
+                        }
+                    }
+                }
+                if (value == null) {
+                    throw new NoSuchElementException();
+                }
+                return value;
+            }
+        };
+    }
+}
