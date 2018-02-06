91d7bfca7ab507db02102d59bda7d038c356e529
zoxy1 <zoxy1@yandex.ru>
2018-02-06 18:41:43 +0700

    add SimpleArray class and tests

---
+++ chapter_001/src/main/java/Generic/SimpleArray.java
@@ -0,0 +1,55 @@
+package Generic;
+
+public class SimpleArray<T> {
+    private Object[] array = new Object[32];
+    private int index = 0;
+    private int size = 0;
+
+    public SimpleArray(Object[] array) {
+        this.array = array;
+        size = array.length;
+    }
+
+    public SimpleArray() {
+    }
+
+    public Boolean add(T model) {
+        if (index >= array.length) {
+            Object[] arrayNew = new Object[(array.length * 2)];
+            System.arraycopy(array, 0, arrayNew, 0, array.length);
+            array = arrayNew;
+        }
+        array[index++] = model;
+        size++;
+        return true;
+    }
+
+    public void set(int index, T model) {
+        rangeCheck(index);
+        array[index] = model;
+    }
+
+    public T delete(int index) {
+        rangeCheck(index);
+        T oldValue = (T) array[index];
+
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(array, index + 1, array, index, numMoved);
+        }
+        size--;
+        return oldValue;
+
+    }
+
+    public T get(int index) {
+        rangeCheck(index);
+        return (T) array[index];
+    }
+
+    private void rangeCheck(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Max index is: " + (size - 1));
+        }
+    }
+}
---
+++ chapter_001/src/test/java/Generic/SimpleArrayTest.java
@@ -0,0 +1,65 @@
+package Generic;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleArrayTest {
+    @Test
+    public void add() throws Exception {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>();
+        simpleArray.add(5);
+        simpleArray.add(6);
+        assertThat(simpleArray.get(0), is(5));
+        assertThat(simpleArray.get(1), is(6));
+    }
+
+    @Test
+    public void set() throws Exception {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(new Integer[]{1, 2, 3, 4});
+        simpleArray.set(0, 10);
+        simpleArray.set(1, 11);
+        assertThat(simpleArray.get(0), is(10));
+        assertThat(simpleArray.get(1), is(11));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void setToNonexistentIndexElementThrowException() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(new Integer[]{1, 2, 3, 4});
+        simpleArray.set(4, 11);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(new Integer[]{1, 2, 3, 4});
+        simpleArray.delete(1);
+        assertThat(simpleArray.get(0), is(1));
+        assertThat(simpleArray.get(1), is(3));
+        assertThat(simpleArray.get(2), is(4));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void deleteNonexistentIndexElementThrowException() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(new Integer[]{1, 2, 3, 4});
+        simpleArray.delete(4);
+    }
+
+    @Test
+    public void get() throws Exception {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(new Integer[]{1, 2, 3, 4});
+        assertThat(simpleArray.get(0), is(1));
+        assertThat(simpleArray.get(1), is(2));
+        assertThat(simpleArray.get(2), is(3));
+        assertThat(simpleArray.get(3), is(4));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getNonexistentIndexElementThrowException() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(new Integer[]{1, 2, 3, 4});
+        simpleArray.get(4);
+    }
+}
