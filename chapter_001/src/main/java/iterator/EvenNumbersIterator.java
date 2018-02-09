71ef9aaa91e72da25acfb9a6133f23870aad1c74
Andrey Kudryavtsev <zoxy1@yandex.ru>
2018-02-10 01:10:45 +0700

    change EvenNumbersIterator class remove duplicate code

--- chapter_001/src/main/java/iterator/EvenNumbersIterator.java
+++ chapter_001/src/main/java/iterator/EvenNumbersIterator.java
@@ -13,31 +13,30 @@ public class EvenNumbersIterator implements Iterator {
 
     @Override
     public boolean hasNext() {
-        Boolean isFound = false;
-
-        if (values.length > index) {
-            for (int i = index; i < values.length; i++) {
-                if (values[i] % 2 == 0) {
-                    isFound = true;
-                }
-            }
-        }
-        return isFound;
+        return (nextEvenValue(IndexUp.NO) != null);
     }
 
     @Override
     public Integer next() {
-        Integer value = null;
-
-        for (int i = index; i < values.length; i++) {
-            index++;
-            if (values[i] % 2 == 0) {
-                value = values[i];
-                break;
-            }
-        }
+        Integer value = nextEvenValue(IndexUp.YES);
         if (value == null) {
             throw new NoSuchElementException();
+        }
+        return value;
+    }
+
+    private Integer nextEvenValue(IndexUp indexUp) {
+        Integer value = null;
+        if (values.length > index) {
+            for (int i = index; i < values.length; i++) {
+                if (indexUp == IndexUp.YES) {
+                    index++;
+                }
+                if (values[i] % 2 == 0) {
+                    value = values[i];
+                    break;
+                }
+            }
         }
         return value;
     }
---
+++ chapter_001/src/main/java/iterator/IndexUp.java
@@ -0,0 +1,9 @@
+package iterator;
+
+/**
+ * Created by Andrey on 10.02.18.
+ */
+public enum IndexUp {
+    YES,
+    NO
+}
