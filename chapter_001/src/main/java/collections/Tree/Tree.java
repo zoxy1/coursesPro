97b0d36f7287a5c5c67f88c0e1956f37d7c0a43b
zoxy1 <zoxy1@yandex.ru>
2018-03-13 18:38:20 +0700

    implements Tree

--- chapter_001/src/main/java/collections/Tree/Tree.java
+++ chapter_001/src/main/java/collections/Tree/Tree.java
@@ -1,5 +1,79 @@
 package collections.Tree;
 
-public class Tree<T extends Comparable<T>> {
-    Node<T> root;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+    private Node<E> root;
+
+    public Tree(E value) {
+        this.root = new Node<>(value);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(parent)) {
+                el.add(new Node<>(child));
+                return true;
+            }
+            for (Node<E> childAdd : el.leaves()) {
+                data.offer(childAdd);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+//                Queue<Node<E>> data = new LinkedList<>();
+//                data.offer(root);
+//                while (!data.isEmpty()) {
+//                    Node<E> el = data.poll();
+//                    if (!el.leaves().isEmpty()) {
+//                        return true;
+//                    }
+//                    for (Node<E> childAdd : el.leaves()) {
+//                        if (!childAdd.leaves().isEmpty()) {
+//                            return true;
+//                        }
+//                        data.offer(childAdd);
+//                    }
+//                }
+                return false;
+            }
+
+            @Override
+            public E next() {
+
+                return null;
+            }
+        };
+    }
 }
