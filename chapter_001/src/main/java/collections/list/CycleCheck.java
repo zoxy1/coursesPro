1330f765e2727b75def0a69991cf184b8337c65f
zoxy1 <zoxy1@yandex.ru>
2018-03-13 15:55:08 +0700

    changed hasCycle() without array

--- chapter_001/src/main/java/collections/list/CycleCheck.java
+++ chapter_001/src/main/java/collections/list/CycleCheck.java
@@ -1,20 +1,21 @@
 package collections.list;
 
 public class CycleCheck {
-
     boolean hasCycle(Node first) {
-        Node[] nodes = new Node[5];
-        nodes[0] = first;
-        for (int i = 1; i < 5; i++) {
-            nodes[i] = nodes[i - 1].next;
-            if (nodes[i] == null) {
-                return false;
-            }
-            for (int j = 0; j < 5; j++) {
-                if (j != i && nodes[i] == nodes[j]) {
+        Node nodeA = first;
+        Node nodeB = nodeA.next;
+        for (int index = 0; index < 4; index++) {
+            for (int i = 0; i < (4 - index); i++) {
+                if (nodeB == null) {
+                    return false;
+                }
+                if (nodeA == nodeB) {
                     return true;
                 }
+                nodeB = nodeB.next;
             }
+            nodeA = nodeA.next;
+            nodeB = nodeA.next;
         }
         return false;
     }
